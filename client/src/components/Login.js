a5ee41a235e49c8c11b7befbdcd449b1b0b304ed
Seth Nadu <seth.nadu@gmail.com>
2019-08-23 11:15:58 -0400

    privateroute/axioswithauth/app

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,24 @@
 import React from "react";
+import {withRouter} from "react-router-dom";
+import axios from "axios";
+import {Form, Field, withFormik} from "formik";
+import * as Yup from 'yup';
 
-const Login = () => {
+const Login = ({errors, touched}) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <Form>
+        <h2>Login</h2>
+        <Field type = "text" name = "username" placeholder ="Username" />
+        {touched.username && errors.username && <p>{errors.username}</p>}
+        <Field type = "password" name = "password" placeholder = "Password" />
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <button type="submit">Submit</button>
+        </Form>
     </>
+  
   );
 };
 
-export default Login;
